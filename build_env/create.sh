7bd9136092814aa4ca0b99a91142784c5cefd4c2
traxsdata <matthewmccarty2006@gmail.com>
2023-06-15 02:36:08 +0000

    7

--- build_env/create.sh
+++ build_env/create.sh
@@ -45,4 +45,40 @@ echo "Creating build enviorement...."
 ## bash -e build_scripts/buildwhat.tar.sh diffutils-3.9.tar.xz diffutils.sh
 
 # https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/file.html
-bash -e build_scripts/buildwhat.tar.sh file-5.44.tar.gz file.sh
+## bash -e build_scripts/buildwhat.tar.sh file-5.44.tar.gz file.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/findutils.html
+## bash -e build_scripts/buildwhat.tar.sh findutils-4.9.0.tar.xz findutils.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/gawk.html
+## bash -e build_scripts/buildwhat.tar.sh gawk-5.2.1.tar.xz gawk.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/grep.html
+## bash -e build_scripts/buildwhat.tar.sh grep-3.8.tar.xz grep.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/gzip.html
+## bash -e build_scripts/buildwhat.tar.sh gzip-1.12.tar.xz gzip.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/make.html
+## bash -e build_scripts/buildwhat.tar.sh make-4.4.tar.gz make.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/patch.html
+## bash -e build_scripts/buildwhat.tar.sh patch-2.7.6.tar.xz patch.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/sed.html
+## bash -e build_scripts/buildwhat.tar.sh sed-4.9.tar.xz sed.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/tar.html
+## bash -e build_scripts/buildwhat.tar.sh tar-1.34.tar.xz tar.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/xz.html
+## bash -e build_scripts/buildwhat.tar.sh xz-5.4.1.tar.xz xz.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/binutils-pass2.html
+## bash -e build_scripts/buildwhat.tar.sh binutils-2.40.tar.xz binutil.p2.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/gcc-pass2.html
+bash -e build_scripts/extract.tar.sh mpfr-4.2.0.tar.xz mpfr gcc-12.2.0.tar.xz
+bash -e build_scripts/extract.tar.sh gmp-6.2.1.tar.xz gmp gcc-12.2.0.tar.xz
+bash -e build_scripts/extract.tar.sh mpc-1.3.1.tar.gz mpc gcc-12.2.0.tar.xz
+bash -e build_scripts/buildwhat.tar.sh gcc-12.2.0.tar.xz gcc.p2.sh
