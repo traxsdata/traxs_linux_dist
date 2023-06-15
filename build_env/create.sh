d04bd4389123a72ab5aad09e38effa3b8cf2b397
traxsdata <matthewmccarty2006@gmail.com>
2023-06-15 02:12:45 +0000

    6.8

--- build_env/create.sh
+++ build_env/create.sh
@@ -13,10 +13,36 @@ echo "Creating build enviorement...."
 ## bash -e build_scripts/buildwhat.tar.sh binutils-2.40.tar.xz binutil.p1.sh
 
 # https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter05/gcc-pass1.html
-bash -e build_scripts/extract.tar.sh mpfr-4.2.0.tar.xz mpfr gcc-12.2.0.tar.xz
-bash -e build_scripts/extract.tar.sh gmp-6.2.1.tar.xz gmp gcc-12.2.0.tar.xz
-bash -e build_scripts/extract.tar.sh mpc-1.3.1.tar.gz mpc gcc-12.2.0.tar.xz
-bash -e build_scripts/buildwhat.tar.sh gcc-12.2.0.tar.xz gcc.p1.sh
+## bash -e build_scripts/extract.tar.sh mpfr-4.2.0.tar.xz mpfr gcc-12.2.0.tar.xz
+## bash -e build_scripts/extract.tar.sh gmp-6.2.1.tar.xz gmp gcc-12.2.0.tar.xz
+## bash -e build_scripts/extract.tar.sh mpc-1.3.1.tar.gz mpc gcc-12.2.0.tar.xz
+## bash -e build_scripts/buildwhat.tar.sh gcc-12.2.0.tar.xz gcc.p1.sh
 
 # https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter05/linux-headers.html
-bash -e build_scripts/buildwhat.tar.sh linux-6.1.11.tar.xz linuxapi.sh
+## bash -e build_scripts/buildwhat.tar.sh linux-6.1.11.tar.xz linuxapi.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter05/glibc.html
+## bash -e build_scripts/buildwhat.tar.sh glibc-2.37.tar.xz glibc.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter05/gcc-libstdc++.html
+## bash -e build_scripts/buildwhat.tar.sh gcc-12.2.0.tar.xz libstdc.sh
+
+
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/m4.html
+## bash -e build_scripts/buildwhat.tar.sh m4-1.4.19.tar.xz m4.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/ncurses.html
+## bash -e build_scripts/buildwhat.tar.sh ncurses-6.4.tar.gz ncurses.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/bash.html
+## bash -e build_scripts/buildwhat.tar.sh bash-5.2.15.tar.gz bash.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/coreutils.html
+## bash -e build_scripts/buildwhat.tar.sh coreutils-9.1.tar.xz coreutils.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/diffutils.html
+## bash -e build_scripts/buildwhat.tar.sh diffutils-3.9.tar.xz diffutils.sh
+
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter06/file.html
+bash -e build_scripts/buildwhat.tar.sh file-5.44.tar.gz file.sh
